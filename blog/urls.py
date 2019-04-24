4e16b4e98cf9779248a84613928c780279368b13
Mars <13808796047@139.com>
2019-04-24 16:41:11 +0800

    建立独立评论应用

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:pk>', views.detail, name='detail'),
+    path('archives/<str:year>/<str:month>', views.archives, name='archives'),
+    path('category/<int:pk>', views.category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 
 
 def index(request):
@@ -17,3 +17,19 @@ def detail(request, pk):
         'markdown.extensions.toc'
     ])
     return render(request, 'blog/detail.html', {'post': post})
+
+
+# 日期分类归档
+def archives(request, year, month):
+    posts = Post.objects.filter(
+        created_time__year=year,
+        created_time__month=month,
+    ).order_by('-created_time')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+# 分类查找
+def category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    posts = Post.objects.filter(category=category).order_by('-created_time')
+    return render(request, 'blog/index.html', {'posts': posts})
